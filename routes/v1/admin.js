d08c311f8accb1c30f08cfdfa22c1f82dc2ad55b
Mattia Avanzi <mattia.avanzi.1@studenti.unipd.it>
2020-08-10 12:12:40 +0200

    Admin test

--- routes/v1/admin.js
+++ routes/v1/admin.js
@@ -12,6 +12,8 @@ router.get('/login', async (req, res) => {
 
 router.post('/api/login', async (req, res) => {
 
+    console.log("bcrypt comparison started");
+
     // TODO: access to DB
     const mail = "m8.avanzi@gmail.com";
     const psw = "$2y$10$qk3QI2LPlf1EH6.rou1Q5.PaM4/gqyoUjDQYpIjxWRnzUWnpMK/fm";
