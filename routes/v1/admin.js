841846de7f777f8c6e7e72e0b3eb21c13c027844
Mattia Avanzi <mattia.avanzi.1@studenti.unipd.it>
2020-08-10 10:44:36 +0200

    Admin test

--- routes/v1/admin.js
+++ routes/v1/admin.js
@@ -1,9 +1,37 @@
 const router = require("express").Router();
+const JWT = require('jsonwebtoken');
+const cookieParser = require('cookie-parser')
 
+app.use(cookieParser())
 //const db = require("../db/config");
 
+
 router.get('/login', async (req, res) => {
-    // completare con query #3
+    res.send('../../public/html/admin-login.html');
+});
+
+router.get('/api/login', async (req, res) => {
+
+    // TODO: SECRET should be an env variable
+    const SECRET = "eqflkbjch9143iuc!";
+    let payload = "email";
+    const token = JWT.token(payload, SECRET);
+	res.cookie("admin_token", token, {
+        maxAge: 3600,
+        httpOnly: true,
+        sameSite: true
+    });
+    res.send('../../public/html/admin-panel.html');
+});
+
+router.get('/panel', async (req, res) => {
+
+    const token = req.cookies.admin_token;
+    // TODO: SECRET should be an env variable
+    const SECRET = "eqflkbjch9143iuc!";
+    const decoded = JWT.verify(token, SECRET);
+
+    res.send('../../public/html/admin-panel.html');
 });
 
 module.exports = router;
