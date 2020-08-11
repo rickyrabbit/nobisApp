c6d049d362050bb9d9b59dc084715f33a8adaf25
Mattia Avanzi <mattia.avanzi.1@studenti.unipd.it>
2020-08-11 10:15:28 +0200

    Admin test

--- db/config.js
+++ db/config.js
@@ -11,12 +11,25 @@ const getAdminByEmail = (request) => {
 
     const email = request.body.email;
 
+    pool.connect((err, client, done) => {
+        if (err) throw err
+        client.query('SELECT * FROM admin WHERE email = $1', [email], (err, res) => {
+            done()
+            if (err) {
+                console.log(err.stack)
+            } else {
+                return res.rows[0];
+            }
+        })
+    })
+/*
     pool.query('SELECT * FROM admin WHERE email = $1', [email], (error, results) => {
         if (error) {
             throw error
         }
         return results.rows[0];
   })
+*/
 }
 
 module.exports = {
