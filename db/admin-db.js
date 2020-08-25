71f3920b55c49061eee04844aa58f7cc62b93788
riccardo <coniglio>
2020-08-25 21:28:24 +0200

    admin dashboard error middleware

--- db/admin-db.js
+++ db/admin-db.js
@@ -2,24 +2,39 @@ const db = require("./config");
 const { QueryError } = require("../routes/errors");
 
 const getAdminByEmail = async (email) => {
-
     try {
         let query = await db.pool.query('SELECT * FROM admin WHERE email = $1', [email]);
         return query.rows[0];
     } catch(e) {
         console.error(e.stack);
-        throw new QueryError();
+        let qe = new QueryError();
+        qe.setReason("QUERY_ADMININFO");
+        throw qe;
     }
 }
 
+/**
+ * 
+ * @param {*} email ee
+ * @param {*} password ee
+ * @async
+ * @throws {QueryError}
+ */
 const checkAdminCredentials = async (email, password) => {
-
     try {
         let query = await db.pool.query("SELECT (password = crypt($2, password)) AS valid FROM admin WHERE email = $1", [email, password]);
-        return query.rows[0].valid;
+        
+        if(query.rows[0] == undefined){
+            // email is mistyped
+            return false;
+        }else{
+            return query.rows[0].valid;
+        }
     } catch(e) {
         console.error(e.stack);
-        throw new QueryError();
+        let qe = new QueryError();
+        qe.setReason("QUERY_ADMINCREDCHECK");
+        throw qe;
     }
 }
 
--- routes/v1/admin.js
+++ routes/v1/admin.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
 const JWT = require('jsonwebtoken');
 
+const { UnAuthenticatedError, InternalServerError, InsertError, DeleteError, UpdateError, QueryError } = require("../errors");
+
 const db = require("../../db/admin-db");
 const referentdb = require("../../db/referent-db");
+
+let wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 router.get('/login', async (req, res) => {
     res.render('admin-login', {
@@ -12,9 +16,10 @@ router.get('/login', async (req, res) => {
     });
 });
 
-router.get('/panel', async (req, res) => {
+router.get('/panel', wrap(async (req, res) => {
     try {
         JWT.verify(req.cookies.admin_token, process.env.ADMIN_SECRET);
+        // TODO: handle these two requests
         let newRef = await referentdb.listNewReferents();
         let oldRef = await referentdb.listOldReferents();
         res.render('admin-panel', {
@@ -24,13 +29,32 @@ router.get('/panel', async (req, res) => {
             css: ['admin-panel']
         });
     } catch (error) {
-        res.status(401).redirect('/admin/login');
-    }
-});
+        console.debug(err);
+        if (err instanceof UpdateError ||err instanceof DeleteError || err instanceof InsertError || err instanceof QueryError ){
+            let ise = new InternalServerError();
+            next(ise);
+        }else if (err instanceof JWT.TokenExpiredError || err instanceof JWT.JsonWebTokenError || err instanceof JWT.NotBeforeError) {
+            // Problems with jwt verify
+            console.log(`problems with jwt token, error: ${err.name}`);
+            console.log(`ekrjbglaevlebvlaeb: ${err.name}`);
 
-router.post('/checkCredentials', async (req, res) => {
+            let unauth = new UnAuthenticatedError();
+            let message = "JWTERROR";
+            //let message = "GotoLogin";
+            unauth.setReason(message);
+            next(unauth);
+        }
+        /* res.status(401).redirect('/admin/login'); */
+        
+    
+        next(err);
+    }
+}));
+
+router.post('/checkCredentials', wrap(async (req, res, next) => {
     try {
-        if (await db.checkAdminCredentials(req.body.email, req.body.password)) {
+        let goodCredentials = await db.checkAdminCredentials(req.body.email, req.body.password);
+        if (goodCredentials) {
             let multiplier = 1;
             if (req.body.remember == "on") 
                 multiplier = 24;
@@ -39,14 +63,57 @@ router.post('/checkCredentials', async (req, res) => {
                 httpOnly: true,
                 sameSite: true
             });
-            res.redirect('/admin/panel');
+            res.status(200).redirect('/admin/panel');
         } else {
-            let message = "Credenziali non valide, per favore riprova.";
-            res.status(401).redirect(`/admin/login?error=${message}`);
+            let ue = new UnAuthenticatedError();
+            console.debug(`siamo qui hahaahaahaha`);
+            let message2 = "Credenziali non valide, per favore riprova.";
+            let message = "WRONGADMINCREDENTIALS";
+            ue.setReason(message);
+            throw ue;
+            /* let message = "Credenziali non valide, per favore riprova.";
+            res.status(401).redirect(`/admin/login?error=${message}`); */
         }
     } catch (err) {
-        let message = "Credenziali non valide, per favore riprova.";
-        res.status(401).redirect(`/admin/login?error=${message}`);
+        if (err instanceof QueryError) {
+            let ise = new InternalServerError();
+            if(err.reason !== ""){
+                ise.setReason(err.reason);
+            }
+            next(ise);
+        }
+        next(err);
+        /* let message = "Credenziali non valide, per favore riprova.";
+        res.status(401).redirect(`/admin/login?error=${message}`); */
+    }
+}));
+
+router.use(function (err, req, res, next) {
+    if (err instanceof UnAuthenticatedError) {
+        console.log(`arriva qui?UnAuthenticatedError`);
+        res.clearCookie("referent_token");
+        res.status(err.statusCode);
+        
+        if(err.reason == 'JWTERROR'){
+            res.redirect('/admin/login');
+        }
+        else if(err.reason == 'WRONGADMINCREDENTIALS'){
+            let message2 = "Credenziali non valide, per favore riprova.";
+            res.redirect(`/admin/login?error=${message2}`);
+        }
+        console.log(`UnAuthenticated Error: error ${err.statusCode}`);
+        return;
+    }
+    else if(err instanceof InternalServerError) {
+        res.status(err.statusCode);
+        console.log(`arriva qui?InternalServerError`);
+        console.log(`Internal server error: error ${err.statusCode}`);
+        // err coming from checkAdminCredentials
+        if(err.reason === "QUERY_ADMINCREDCHECK"){
+            res.redirect(`/admin/login`);
+        }
+        //res.redirect(`/admin/login`);
+        return;
     }
 });
 
