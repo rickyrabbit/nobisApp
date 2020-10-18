13206716197155ac025e534d1f8b868940f144ce
Mattia Avanzi <m8.avanzi@gmail.com>
2020-10-18 18:59:08 +0200

    New Cron to save log

---
+++ db/crons/visit.sql
@@ -0,0 +1,1 @@
+select place.name AS posto,visit.person_uuid AS persona,log.is_in AS èdentroStanza, assumption AS dedotto, timestamp AS istante from visit left join log  on log.id = visit.log_id left join place on place.uuid = visit.place_uuid group by posto,persona,istante,èdentroStanza,dedotto order by timestamp desc
